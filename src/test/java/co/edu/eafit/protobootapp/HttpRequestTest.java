158ceafca77b187d42cfa37b9f45bf6da63ee87b
leonj <leonj@192.168.1.4>
2021-03-25 17:54:28 -0500

    Las pruebas de integración usan regex.

--- src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
+++ src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
@@ -29,24 +29,24 @@ public class HttpRequestTest {
     @Test
     public void testCuadradoPorDefecto() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cuadrado",
-                String.class)).contains("El cuadrado de 0,000000 es 0,000000");
+                String.class)).containsPattern("El cuadrado de 0[\\.,]000000 es 0[\\.,]000000");
     }
     
     @Test
     public void testCuboPorDefecto() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cubo",
-                String.class)).contains("El cubo de 0,000000 es 0,000000");
+                String.class)).containsPattern("El cubo de 0[\\.,]000000 es 0[\\.,]000000");
     }
     
     @Test
     public void testCuadradoNumero() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cuadrado?numero=3",
-                String.class)).contains("El cuadrado de 3,000000 es 9,000000");
+                String.class)).containsPattern("El cuadrado de 3[\\.,]000000 es 9[\\.,]000000");
     }
     
     @Test
     public void testCuboNumero() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/cubo?numero=3",
-                String.class)).contains("El cubo de 3,000000 es 27,000000");
+                String.class)).containsPattern("El cubo de 3[\\.,]000000 es 27[\\.,]000000");
     }
 }
