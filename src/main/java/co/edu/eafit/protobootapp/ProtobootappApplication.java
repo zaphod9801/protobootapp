5b12038f5503494bc701a42e108568340f259d46
leonj <leonj@192.168.1.4>
2021-03-25 16:12:14 -0500

    Se agregó un comentario que recuerda el propósito de la app.

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//App para hallar el cuadrado y el cubo de un número
 //Se lanza y se ejecuta con http://localhost:8080
 @SpringBootApplication
 @RestController
