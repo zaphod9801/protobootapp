9df24fd5a50e0b3454cead5ad316f5bb2a72e496
leonj <leonj@192.168.1.4>
2021-03-25 21:20:30 -0500

    Se cambió de nuevo el saludo de la aplicación.

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Esta aplicación lo saluda y le da el cuadrado y el cubo de un número.");
+        return String.format("Bienvenido! Consulte el modo de uso de esta aplicación con el administrador.");
     }    
 
 //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
