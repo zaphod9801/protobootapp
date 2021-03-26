50a983f093652ff3a382a887cd093ed220ea4e37
leonj <leonj@192.168.1.4>
2021-03-26 18:57:11 -0500

    Cambié el saludo.

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -21,7 +21,7 @@ public class ProtobootappApplication {
     //Se ejecuta con http://localhost:8080/
     @GetMapping("/")
     public String index() {
-        return String.format("Bienvenido! Consulte el modo de uso de esta aplicación con el administrador.");
+        return String.format("Bienvenido! Esta aplicación ayuda a elevar potencias.");
     }    
 
 //Se ejecuta con http://localhost:8080/hola o http://localhost:8080/hola?nombre=John
