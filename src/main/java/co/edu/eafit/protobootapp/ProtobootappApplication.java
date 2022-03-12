675a670cce4fd1b59545e1dad3c5eb6f91bc92d0
leonj <leonj@192.168.1.4>
2022-03-12 10:14:29 -0500

    Fixed comment.

--- src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
+++ src/main/java/co/edu/eafit/protobootapp/ProtobootappApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //App para hallar el cuadrado y el cubo de un número
-//Se lanza y se ejecuta con http://localhost:8080
+//Se lanza y se ejecuta con http://localhost:8080 o la URL específica al proveedor cloud
 @SpringBootApplication
 @RestController
 public class ProtobootappApplication {
