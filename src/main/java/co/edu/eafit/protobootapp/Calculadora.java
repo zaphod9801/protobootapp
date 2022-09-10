42ee25edbb53fe99907249abd462818fe67acec9
leonj <leonj@192.168.1.4>
2022-09-09 20:59:09 -0500

    Adding in cube.

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,10 +3,10 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero + numero;
+        return numero * numero;
     }
     
     public Double cubo(Double numero) {
-        return numero * numero * numero;
+        return numero + numero + numero;
     }
 }
