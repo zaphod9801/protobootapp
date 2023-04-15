f973912ea25d013f3cbd192216d07c5f0b3cb84e
zaphod9801 <johanruiz932@gmail.com>
2023-04-15 09:23:27 -0500

    cube failed

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -6,8 +6,8 @@ public class Calculadora {
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
-    
+
     public Double cubo(Double numero) {
-        return numero * numero * numero;
+        return numero * numero + numero;
     }
 }
